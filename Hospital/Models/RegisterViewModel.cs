9f43271d7015e56394c6d7dea80c667fbf296bab
Albarn <elrib7vj@gmail.com>
2019-03-08 13:49:24 +0200

    update register form validation

--- Hospital/Models/RegisterViewModel.cs
+++ Hospital/Models/RegisterViewModel.cs
@@ -9,13 +9,16 @@ namespace Hospital.Models
     public class RegisterViewModel
     {
         [Required]
+        [RegularExpression(@"^[A-Za-z0-9_@\.]+$", 
+            ErrorMessage = "Only alphabetical letters, digits and _, @, . characters allowed.")]
         public string UserName { get; set; }
         [Required]
         [DataType(DataType.Password)]
+        [StringLength(20,ErrorMessage = "Password must contain from 6 to 20 characters.", MinimumLength = 6)]
         public string Password { get; set; }
         [Required]
         [DataType(DataType.Password)]
-        [Compare("Password")]
+        [Compare("Password", ErrorMessage = "Passwords do not match.")]
         public string ConfirmPassword { get; set; }
     }
 }
